2fcf436b740bba4f610bbf2ed8e93b1072e84f83
Jackson Ming Hu <huming2207@gmail.com>
2019-04-23 21:46:50 +1000

    fp1020a: implement major functions

--- fp1020a.c
+++ fp1020a.c
@@ -8,7 +8,7 @@
 
 #define TAG "fp1020a"
 
-static fp_ack_t fp1020a_send_cmd(const uint8_t *payload)
+static esp_err_t fp1020a_send_cmd(const uint8_t *payload)
 {
     uint8_t checksum = 0;
     for(uint8_t curr_idx = 0; curr_idx < 5; curr_idx++) {
@@ -16,7 +16,12 @@ static fp_ack_t fp1020a_send_cmd(const uint8_t *payload)
     }
 
     uint8_t data[] = { 0xf5, payload[0], payload[1], payload[2], payload[3], payload[4], checksum, 0xf5 };
-    uart_write_bytes(UART_NUM_1, (const char *)data, sizeof(data));
+    return uart_write_bytes(UART_NUM_1, (const char *)data, sizeof(data)) > 0 ? ESP_OK : ESP_FAIL;
+}
+
+static esp_err_t fp1020a_recv_ack(uint8_t *out, size_t len)
+{
+    return uart_read_bytes(UART_NUM_1, out, len, pdMS_TO_TICKS(10000)) > 0 ? ESP_OK : ESP_FAIL;
 }
 
 void fp1020a_init()
@@ -34,4 +39,193 @@ void fp1020a_init()
     uart_param_config(UART_NUM_1, &uart_config);
     uart_set_pin(UART_NUM_1, CONFIG_FP1020A_TX_PIN, CONFIG_FP1020A_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
     uart_driver_install(UART_NUM_1, CONFIG_FP1020A_BUFFER_SIZE, 0, 0, NULL, 0);
+}
+
+void fp1020a_sleep()
+{
+    uint8_t sleep_cmd[] = { FP1020A_CMD_SLEEP, 0, 0, 0, 0 };
+    fp1020a_send_cmd(sleep_cmd);
+}
+
+fp_ack_t fp1020a_add_fp(const uint8_t stage, const uint16_t user_id, const fp_user_lvl_t user_lvl)
+{
+    uint8_t fp_cmd_data[] = {
+                stage,
+                (user_id >> 8u),            // User ID's MSB
+                (user_id & 0xffU),          // User ID's LSB
+                user_lvl,
+                0
+            };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_ACK_UART_ERROR;
+    return fp_add_result[3]; // If nothing goes wrong, return the status byte.
+}
+
+fp_ack_t fp1020a_add_fp_start(const uint16_t user_id, const fp_user_lvl_t user_lvl)
+{
+    return fp1020a_add_fp(FP1020A_CMD_ADD_FINGER_1, user_id, user_lvl);
+}
+
+fp_ack_t fp1020a_add_fp_intermediate(const uint16_t user_id, const fp_user_lvl_t user_lvl)
+{
+    return fp1020a_add_fp(FP1020A_CMD_ADD_FINGER_2, user_id, user_lvl);
+}
+
+fp_ack_t fp1020a_add_fp_end(const uint16_t user_id, const fp_user_lvl_t user_lvl)
+{
+    return fp1020a_add_fp(FP1020A_CMD_ADD_FINGER_3, user_id, user_lvl);
+}
+
+fp_ack_t fp1020a_remove_one_user(const uint16_t user_id)
+{
+    uint8_t fp_cmd_data[] = {
+            FP1020A_CMD_DELETE_USER,
+            (user_id >> 8u),            // User ID's MSB
+            (user_id & 0xffU),          // User ID's LSB
+            0,
+            0
+    };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_ACK_UART_ERROR;
+    return fp_add_result[3]; // If nothing goes wrong, return the status byte.
+}
+
+fp_ack_t fp1020a_remove_all_user()
+{
+    uint8_t fp_cmd_data[] = { FP1020A_CMD_DELETE_ALL, 0, 0, 0, 0 };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_ACK_UART_ERROR;
+    return fp_add_result[3]; // If nothing goes wrong, return the status byte.
+}
+
+int fp1020a_get_user_amount()
+{
+    uint8_t fp_cmd_data[] = { FP1020A_CMD_USER_AMOUNT, 0, 0, 0, 0 };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_ACK_UART_ERROR;
+
+    uint16_t user_count = ((uint16_t)(fp_add_result[1] << 8u) | fp_add_result[2]);
+    return user_count;
+}
+
+fp_ack_t fp1020a_comp_one_to_one(const uint16_t user_id)
+{
+    uint8_t fp_cmd_data[] = {
+            FP1020A_CMD_COMP_USER_ID,
+            (user_id >> 8u),            // User ID's MSB
+            (user_id & 0xffU),          // User ID's LSB
+            0,
+            0
+    };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_ACK_UART_ERROR;
+    return fp_add_result[3]; // If nothing goes wrong, return the status byte.
+}
+
+fp_auth_t *fp1020a_auth_user()
+{
+    uint8_t fp_cmd_data[] = { FP1020A_CMD_AUTH_USER, 0, 0, 0, 0 };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return NULL;
+
+    fp_auth_t *fp_auth = malloc(sizeof(fp_auth_t));
+
+    if(fp_add_result[3] == FP1020A_ACK_NOUSER || fp_add_result[3] == FP1020A_ACK_TIMEOUT) {
+        fp_auth->level = FP1020A_USER_LEVEL_FAIL;
+        fp_auth->user_id = 0;
+    } else {
+        fp_auth->level = (fp_user_lvl_t)fp_add_result[3];
+        fp_auth->user_id = ((uint16_t)(fp_add_result[1] << 8u) | fp_add_result[2]);
+    }
+
+    return fp_auth;
+}
+
+fp_user_lvl_t fp1020a_get_user_level(const uint16_t user_id)
+{
+    uint8_t fp_cmd_data[] = {
+            FP1020A_CMD_GET_USER_LVL,
+            (user_id >> 8u),            // User ID's MSB
+            (user_id & 0xffU),          // User ID's LSB
+            0,
+            0
+    };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK || fp_add_result[3] == FP1020A_ACK_NOUSER)
+        return FP1020A_USER_LEVEL_FAIL;
+
+    return (fp_user_lvl_t)fp_add_result[3];
+}
+
+fp_ack_t fp1020a_set_comp_level(uint8_t level)
+{
+    uint8_t fp_cmd_data[] = {
+            FP1020A_CMD_SET_COMP_LEVEL,
+            0,
+            level,      // Compare level
+            1,          // 1 for writing settings
+            0
+    };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_ACK_UART_ERROR;
+    return (fp_ack_t)fp_add_result[3];
+}
+
+fp_baud_rate_t fp1020a_set_baud_rate(fp_baud_rate_t baud_rate)
+{
+    uint8_t fp_cmd_data[] = {
+            FP1020A_CMD_SET_BAUD_RATE,
+            0,
+            0,
+            baud_rate,
+            0
+    };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return FP1020A_BAUD_RATE_UNKNOWN;
+    return (fp_baud_rate_t)fp_add_result[3];
+}
+
+int fp1020a_get_next_avail_user_id(const uint16_t id_begin, const uint16_t id_end)
+{
+    uint8_t fp_cmd_data[] = {
+            FP1020A_CMD_GET_AVALIABLE_USER_ID,
+            (id_begin >> 8u),
+            (id_begin & 0xffU),
+            (id_end >> 8u),
+            (id_end & 0xffU)
+    };
+
+    fp1020a_send_cmd(fp_cmd_data);
+
+    uint8_t fp_add_result[5] = { 0 };
+    if(fp1020a_recv_ack(fp_add_result, 5) != ESP_OK) return -1;
+    if(fp_add_result[3] == FP1020A_ACK_NOUSER || fp_add_result[3] == FP1020A_ACK_FAIL) return -2;
+    return ((uint16_t)(fp_add_result[1] << 8u) | fp_add_result[2]);
 }
--- include/fp1020a.h
+++ include/fp1020a.h
@@ -20,7 +20,7 @@
 #define FP1020A_ACK_USER_EXIST              0x07        // User exist
 #define FP1020A_ACK_TIMEOUT                 0x08        // Timeout
 #define FP1020A_ACK_TRY_AGAIN               0x09        // Try again (when no finger is pressed for authentication)
-#define FP1020A_ACK_UART_TIMEOUT            -1          // UART timeout
+#define FP1020A_ACK_UART_ERROR            -1          // UART timeout
 
 #define FP1020A_CMD_ADD_FINGER_1            0x01        // Add a fingerprint (the first touch)
 #define FP1020A_CMD_ADD_FINGER_2            0x02        // Add a fingerprint (the 2nd to 5th touches)
@@ -28,7 +28,7 @@
 #define FP1020A_CMD_DELETE_USER             0x04        // Delete a single user
 #define FP1020A_CMD_DELETE_ALL              0x05        // Delete all users
 #define FP1020A_CMD_USER_AMOUNT             0x09        // Get the user amount
-#define FP1020A_CMD_GET_USER_PERMISSION     0x0A        // Get a particular user's permission level (1-3)
+#define FP1020A_CMD_GET_USER_LVL            0x0A        // Get a particular user's permission level (1-3)
 #define FP1020A_CMD_COMP_USER_ID            0x0B        // Compare a fingerprint with an user ID
 #define FP1020A_CMD_AUTH_USER               0x0C        // Compare a fingerprint to all existing users in the database
 #define FP1020A_CMD_SET_BAUD_RATE           0x21        // Set baud rate
@@ -49,10 +49,131 @@
 #define FP1020A_CMD_COMP_HASH_FINGER        0x44        // Upload & compare a hash with a finger
 #define FP1020A_CMD_GET_AVALIABLE_USER_ID   0x47        // Get the next available user ID
 
-#define FP1020A_BAUD_RATE_9600              0x01
-#define FP1020A_BAUD_RATE_19200             0x02        // Default setting
-#define FP1020A_BAUD_RATE_38400             0x03
-#define FP1020A_BAUD_RATE_57600             0x04
-#define FP1020A_BAUD_RATE_115200            0x05
 
 typedef int8_t fp_ack_t;
+
+typedef enum {
+    FP1020A_USER_LEVEL_FAIL = -1,
+    FP1020A_USER_LEVEL_1 = 1,
+    FP1020A_USER_LEVEL_2 = 2,
+    FP1020A_USER_LEVEL_3 = 3
+} fp_user_lvl_t;
+
+typedef struct {
+    fp_user_lvl_t level;
+    uint16_t user_id;
+} fp_auth_t;
+
+typedef enum {
+    FP1020A_BAUD_RATE_UNKNOWN = -1,
+    FP1020A_BAUD_RATE_9600 = 1,
+    FP1020A_BAUD_RATE_19200 = 2,         // Default setting
+    FP1020A_BAUD_RATE_38400 = 3,
+    FP1020A_BAUD_RATE_57600 = 4,
+    FP1020A_BAUD_RATE_115200 = 5
+} fp_baud_rate_t;
+
+/**
+ * Initialise UART
+ */
+void fp1020a_init();
+
+/**
+ * Put FP1020A to sleep mode
+ */
+void fp1020a_sleep();
+
+/**
+ * Register a fingerprint
+ * @param stage one of the FP1020A_CMD_ADD_FINGER_1/2/3
+ * @param user_id User ID
+ * @param user_lvl User Level
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_add_fp(uint8_t stage, uint16_t user_id, fp_user_lvl_t user_lvl);
+
+/**
+ * Register a fingerprint - start stage
+ * @param user_id User ID
+ * @param user_lvl User Level
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_add_fp_start(uint16_t user_id, fp_user_lvl_t user_lvl);
+
+/**
+ * Register a fingerprint - intermediate stage (2nd to 5th)
+ * @param user_id User ID
+ * @param user_lvl User Level
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_add_fp_intermediate(uint16_t user_id, fp_user_lvl_t user_lvl);
+
+/**
+ * Register a fingerprint - end stage
+ * @param user_id User ID
+ * @param user_lvl User Level
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_add_fp_end(uint16_t user_id, fp_user_lvl_t user_lvl);
+
+/**
+ * Remove one user by User ID
+ * @param user_id User ID
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_remove_one_user(uint16_t user_id);
+
+/**
+ * Remove all users
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_remove_all_user();
+
+/**
+ * Get user amount
+ * @return -1 if fails; or the user amount (>= 0)
+ */
+int fp1020a_get_user_amount();
+
+/**
+ * Compare a fingerprint currently putting on the sensor with an User ID
+ * @param user_id User ID
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_comp_one_to_one(uint16_t user_id);
+
+/**
+ * Compare all the registered users with a a fingerprint that
+ * currently putting on the sensor
+ * @return Fingerprint sensor ACK
+ */
+fp_auth_t *fp1020a_auth_user();
+
+/**
+ * Get user permission level
+ * @param user_id User ID
+ * @return User permission level
+ */
+fp_user_lvl_t fp1020a_get_user_level(uint16_t user_id);
+
+/**
+ * Set comparison strictness level
+ * @param level 0-9, default is 5
+ * @return Fingerprint sensor ACK
+ */
+fp_ack_t fp1020a_set_comp_level(uint8_t level);
+
+/**
+ * Set UART baud rate
+ * @param baud_rate New baud rate
+ * @return Old baud rate, or fail message if fails
+ */
+fp_baud_rate_t fp1020a_set_baud_rate(fp_baud_rate_t baud_rate);
+
+/**
+ * Get next available user ID
+ * @param id_begin Begin ID range
+ * @param id_end End ID range
+ * @return Next available user ID
+ */
+int fp1020a_get_next_avail_user_id(uint16_t id_begin, uint16_t id_end);
